eb9f841cbd39423df219bea067d022c3daf8a8c6
Andreas Åkerlund (anau) <andreas.akerlund@svt.se>
2018-05-25 17:23:51 +0200

    Upload new versions to internal repo. Beta and release gets uploaded to pypi aswell

--- upload.sh
+++ upload.sh
@@ -1,6 +1,10 @@
 ./bumpversion.py --$1 || exit 1
-python setup.py sdist upload -r pypi
+python setup.py sdist upload -r internal
+if [ "$1" == "beta" ]; then
+ python setup.py sdist upload -r pypi
+fi
 if [ "$1" == "release" ]; then
-./bumpversion.py --patch
+ python setup.py sdist upload -r pypi
+ ./bumpversion.py --patch
 fi
 
