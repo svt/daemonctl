5b9511c62ac294fba398588315b02de4f3f1ed39
Andreas Åkerlund <andreas.akerlund@svt.se>
2019-06-28 09:47:24 +0200

    Aparently setuptools upload isn't compatible with python 2 anymore

--- upload.sh
+++ upload.sh
@@ -1,10 +1,10 @@
 ./bumpversion.py --$1 || exit 1
-python setup.py sdist upload -r internal
+python3 setup.py sdist upload -r internal
 if [ "$1" == "beta" ]; then
- python setup.py sdist upload -r pypi
+ python3 setup.py sdist upload -r pypi
 fi
 if [ "$1" == "release" ]; then
- python setup.py sdist upload -r pypi
+ python3 setup.py sdist upload -r pypi
  ./bumpversion.py --patch
 fi
 
