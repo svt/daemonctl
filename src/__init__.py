3b38031a7c4b2d506fb1c85f85028b34b55ac2d5
Andreas Åkerlund (anau) <andreas.akerlund@svt.se>
2022-11-03 10:18:10 +0100

    Bumped version to 1.3.26a4

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim: ts=4 sw=4 et bg=dark
 
-__version__ = '1.3.26a3'
+__version__ = '1.3.26a4'
 
 appname = "daemonctl"
 appdesc = "Utility and framework for simplifying application deployment and operation"
